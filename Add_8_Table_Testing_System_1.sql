cd01dc4ab17fdc30ad377ec841cd4f99d5da118b
Duong Nhat Minh <nhatminh.st.04@gmail.com>
2025-03-11 18:06:16 +0700

    feat<exercise_1> them 8 bang con thieu

---
+++ Add_8_Table_Testing_System_1.sql
@@ -0,0 +1,58 @@
+use `testing_system_assignment_1` ;
+create table `group`(
+	groupId int primary key auto_increment ,
+    groupName varchar(50) ,
+    creatorId int ,
+    createDate Date ,
+    foreign key(creatorId) references account(accountId)
+);
+create table group_account(
+	groupId int ,
+    accountId int ,
+    joinDate Date ,
+    foreign key(groupId) references `group`(groupId) ,
+    foreign key(accountId) references account(accountId)
+);
+create table type_question(
+	typeId int primary key auto_increment ,
+    typeName Enum ('Essay' , 'Multiple-Choice')
+);
+create table category_question(
+	categoryID int primary key auto_increment ,
+    categoryName varchar(50) 
+);
+create table question(
+	questionID int primary key auto_increment ,
+    content varchar(100) ,
+    categoryId int ,
+    typeId int ,
+    creatorId int ,
+    createDate Date ,
+    foreign key(categoryId) references category_question(categoryID) ,
+    foreign key(typeId) references type_question(typeId) ,
+    foreign key(creatorId) references account(accountId) 
+);
+create table answer (
+	answerId int primary key auto_increment ,
+    content varchar(100) ,
+    questionId int ,
+    isCorrect bool ,
+    foreign key(questionId) references question(questionID) 
+);
+create table exam(
+	examId int primary key auto_increment ,
+    code int ,
+    title varchar(50) ,
+    categoryId int ,
+    duration Time ,
+    creatorId int ,
+    createDate Date ,
+    foreign key(categoryId) references category_question(categoryID) ,
+    foreign key(creatorId) references account(accountId)
+);
+create table exam_question(
+	examId int ,
+    questionId int ,
+    foreign key(examId) references exam(examId) ,
+    foreign key(questionId) references question(questionID)
+);
